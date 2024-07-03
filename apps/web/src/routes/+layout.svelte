b3d4880acb264a22d404a041015fd98b53946ca0
Anton Hughes <kurrent93@gmail.com>
2024-07-03 12:17:19 +1200

    simplify 3

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -3,8 +3,7 @@
 
   import type { Snippet } from 'svelte';
 
-import Footer from './Footer.svelte';
-  import HeaderNavigation from './HeaderNavigation.svelte';
+  import Footer from './Footer.svelte';
 
   let {
     children,
@@ -17,8 +16,6 @@ import Footer from './Footer.svelte';
 
 <div class="h-screen bg-gray-100 text-zinc-900">
   <div class="flex h-full flex-col">
-    <HeaderNavigation />
-
     <div class="flex flex-1 flex-col">
       <main class="flex-1 pt-[68px] px-4 pb-16 md:pt-10">
         {@render children()}
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -2,7 +2,7 @@
   // import { onMount } from 'svelte';
 
   import { PUBLIC_TEST_ENV_VAR } from '$env/static/public';
-  import Meta from '$lib/components/Meta.svelte';
+
   // import Comments from '$lib/features/comment/Comments.svelte';
   // import { commentStore } from '$lib/features/comment/commentStore.svelte';
   // import { userStore } from '$lib/features/user/userStore.svelte';
@@ -11,11 +11,11 @@
   // import CommentForm from './CommentForm.svelte';
   // import LoginMessage from './LoginMessage.svelte';
 
-  const meta = {
-    type: 'website' as const,
-    title: `WebApp Template (web)`,
-    canonical: 'https://webapp-template.usagizmo.com',
-  };
+  // const meta = {
+  //   type: 'website' as const,
+  //   title: `WebApp Template (web)`,
+  //   canonical: 'https://webapp-template.usagizmo.com',
+  // };
 
   // onMount(() => {
   //   commentStore.fetchComments();
@@ -23,5 +23,3 @@
 </script>
 
 <h3>The env var is {PUBLIC_TEST_ENV_VAR}</h3>
-
-<Meta {...meta} />
